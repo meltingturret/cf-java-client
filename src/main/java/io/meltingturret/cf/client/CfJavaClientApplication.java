ac8cf94d4460d97964512cbcdc620bfcde5dbce5
Paul Gilligan <meltingturret@gmail.com>
2022-01-28 22:00:15 +0100

    Init

---
+++ src/main/java/io/meltingturret/cf/client/CfJavaClientApplication.java
@@ -0,0 +1,13 @@
+package io.meltingturret.cf.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/meltingturret/cf/client/CfJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meltingturret.cf.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
